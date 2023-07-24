696f0095c587a485e9bfa9b489316c02241e1bf0
priyankakatreddy <priyankakatreddy54@gmail.com>
2023-07-24 17:37:07 +0000

    added requirments

--- main.py
+++ main.py
@@ -37,7 +37,7 @@ def hello_world():
   
 @app.route("/api/jobs")
 def list_jobs():
- return jsonify(JOBS)
+  return jsonify(JOBS)
 
 if __name__ == '__main__':
    app.run(host='0.0.0.0', debug=True)
